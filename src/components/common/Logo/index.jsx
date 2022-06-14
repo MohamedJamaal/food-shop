e86dccaafa866e05589100f929731069d45e1a2d
MohamedJamaal <mohamed_gamal19@hotmail.com>
2022-06-14 09:59:08 +0200

    ADD Banner page & logo

--- src/components/common/Logo/index.jsx
+++ src/components/common/Logo/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './style.css';
+import logoImg from './logo.png';
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div className="logo">
+      <div>
+        <img src={logoImg} alt="logo" />
+        <span>
+          <b>Taset</b>
+        </span>
+      </div>
+      <p>
+        <b>Restaurant & BBQ</b>
+      </p>
+    </div>
+  );
 };
 
 export default Logo;
