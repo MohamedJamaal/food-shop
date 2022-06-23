fcff63d75d25b96985f65d64ff19ba57deb78e1b
MohamedJamaal <mohamed_gamal19@hotmail.com>
2022-06-23 23:41:21 +0200

    ADD routing & navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Home from './pages/Home';
 import './App.css';
+import {
+  Navigate,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
     <div className="container">
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
