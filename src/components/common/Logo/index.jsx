c5aa15f6f8ccf11f0b29687486adb54c8d44147a
MohamedJamaal <mohamed_gamal19@hotmail.com>
2022-10-14 04:31:18 +0200

    Modify link

--- src/components/common/Logo/index.jsx
+++ src/components/common/Logo/index.jsx
@@ -4,17 +4,19 @@ import logoImg from './logo.png';
 
 const Logo = () => {
   return (
-    <div className="logo">
-      <div>
-        <img src={logoImg} alt="logo" />
-        <span>
-          <b>Taset</b>
-        </span>
+    <a href="/">
+      <div className="logo">
+        <div>
+          <img src={logoImg} alt="logo" />
+          <span>
+            <b>Taset</b>
+          </span>
+        </div>
+        <p>
+          <b>Restaurant & BBQ</b>
+        </p>
       </div>
-      <p>
-        <b>Restaurant & BBQ</b>
-      </p>
-    </div>
+    </a>
   );
 };
 
